5e1ef595d2bba10acef280215e0ab43d2a6aaf49
chris <chris@DESKTOP-36LNQQM.home>
2019-02-09 21:34:49 +0000

    Shut down has been simplified because project paths are managed by the project.

--- AuxiliaryFunctions/ProjectManagement/ProjectShutDown.m
+++ AuxiliaryFunctions/ProjectManagement/ProjectShutDown.m
@@ -8,25 +8,7 @@ clc;
 
 % Close down project
 disp('Closing down project.');
-% Get Project Root
-[ ~ , ~ , projectRoot, ~ ] = GetProjObj;
-
-
-% Remove paths added for this project. Get the single definition of the
-% folders to add to the path:
-folders = ProjectPaths();
-
-% Remove these from the MATLAB path:
-for Folder_Idx=1:numel(folders)
-    disp(['     Removing path: ', fullfile(projectRoot, folders{Folder_Idx})]);
-    rmpath( fullfile(projectRoot, folders{Folder_Idx}) );
-end
 
 % Reset the location where generated code and other temporary files are
 % created (slprj) to the default:
 Simulink.fileGenControl('reset');
-
-% clear up workspace
-close all;
-clear all;
-clc;
